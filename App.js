b0a922a2caad6866ac79b502a2fb9033c4c9b86b
ta <tes@riseup.net>
2022-02-01 00:31:44 +0100

    Refactor code to add React Navigation functionalities.

--- App.js
+++ App.js
@@ -1,44 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { getWeatherByCityId } from './services/services';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigation from './components/MainNavigation';
 
 const App = () => {
-  const [weatherDetail, setWeatherDetail] = useState('');
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    getWeatherByCityId('3564073').then(weatherData => {
-      setWeatherDetail(weatherData);
-      setLoaded(true);
-    });
-  }, []);
-
   return (
-    <View>
-      <Text>Consulta del clima</Text>
-
-      {/* Check if we have weather information  */}
-      {loaded && weatherDetail && (
-        <View>
-          <Text>City: {weatherDetail.name}</Text>
-          <Text>Temp: {weatherDetail.main.temp}</Text>
-          <Text>Temp Max: {weatherDetail.main.temp_max}</Text>
-          <Text>Temp Min: {weatherDetail.main.temp_min}</Text>
-          <Text>Humidity: {weatherDetail.main.humidity}</Text>
-          <Text>Pressure: {weatherDetail.main.pressure}</Text>
-          <Text>Feels: {weatherDetail.main.feels_like}</Text>
-          {/* Iterate over weather details */}
-          {weatherDetail.weather.map(info => {
-            return (
-              <View key={info.id}>
-                <Text>Main: {info.main}</Text>
-                <Text>Description: {info.description}</Text>
-              </View>
-            );
-          })}
-        </View>
-      )}
-    </View>
+    <NavigationContainer>
+      <MainNavigation />
+    </NavigationContainer>
   );
 };
 export default App;
