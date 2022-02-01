643607a4e8cbde95b5b48b0837c3c7906586a09e
ta <tes@riseup.net>
2022-02-01 14:22:19 +0100

    Replace query of current weather data by city Name instead of city Id to reuse city Name information over codebase

--- services/services.js
+++ services/services.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const apiKey = 'appid=499a8b8749089316ff980a5a810ef619';
 
-// Get weather by City ID
-export const getWeatherByCityId = async id => {
-  const resp = await axios.get(`${apiUrl}?id=${id}&${apiKey}`);
+// Get weather by City Name
+export const getWeatherByCityName = async name => {
+  const resp = await axios.get(`${apiUrl}?q=${name}&${apiKey}`);
   return resp.data;
 };
